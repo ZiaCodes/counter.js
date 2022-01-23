4df6ad909f4b98fec8e5848827faefc4881999af
Syed Ziauddin <Syed.nlp00@gmail.com>
2022-01-23 20:19:37 +0530

    added modules

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ app.get('/',(req,res) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Can you hack this server?</title>
+    <title>Visitor Counter</title>
 </head>
 <body>
-<h1>Welcome to the most Secured Server!</h1>
+<h1>Welcome User</h1>
 <p>This page has been viewed ${count} times.</p>
 </body>
 </html>`
